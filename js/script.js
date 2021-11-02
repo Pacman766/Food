38fedc1dc62c1480580a1cfc748cc37a73230634
pacman766 <3334976@gmail.com>
2021-11-02 11:45:21 +0300

    0211 1145

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
-
   // Tabs
 
   const tabs = document.querySelectorAll('.tabheader__item'), // табы
     tabsContent = document.querySelectorAll('.tabcontent'), // картинки табов
     tabsParent = document.querySelector('.tabheader__items'); // родитель табов
 
-  // ф-ция для скрытия табов. Для этого добавляем класс hide(css) и удаляем классы show, fade (css), 
+  // ф-ция для скрытия табов. Для этого добавляем класс hide(css) и удаляем классы show, fade (css),
   // а также убираем класс активности tabheader__item_active
   function hideTabContent() {
     tabsContent.forEach((item) => {
@@ -19,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  // ф-ция для показа первого эл-та табов. Для этого i-му эл-ту добавляем классы show, fade (css) 
+  // ф-ция для показа первого эл-та табов. Для этого i-му эл-ту добавляем классы show, fade (css)
   // и удаляем класс hide (css), а также добавляем класс активности tabheader__item_active
   function showTabContent(i = 0) {
     tabsContent[i].classList.add('show', 'fade');
@@ -30,7 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
   hideTabContent();
   showTabContent();
 
-  // создаем обработчик событий и с помощью делегирования вешаем обработчик на родительский 
+  // создаем обработчик событий и с помощью делегирования вешаем обработчик на родительский
   // класс табов и проверяем, если клик произошел на нужный таб, то вызываем ф-ции hideTabContent и showTabContent.
   tabsParent.addEventListener('click', (event) => {
     const target = event.target;
@@ -52,32 +51,41 @@ window.addEventListener('DOMContentLoaded', () => {
   // Вычисляем оставшееся время с помощью вычитания из конечной даты
   // текущей даты, а также конвертируем миллисекунды в соотв единицы времени.
   // Возвращаем единицы времении в виде объекта
-  function getTimeRemaining (endTime) {
+  function getTimeRemaining(endTime) {
     const t = Date.parse(endTime) - Date.parse(new Date()),
-          days = Math.floor(t / (1000 * 60 * 60 * 24)),
-          hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-          minutes = Math.floor((t / 1000 / 60) % 60),
-          seconds = Math.floor((t / 1000) % 60); 
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      seconds = Math.floor((t / 1000) % 60);
 
     return {
-      'total': t, 
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  // ф-ция для подставления 0 перед числом, если цифра <10
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
     }
   }
-  // Устанавливаем время с помощью соответствующих селекторов 
+  // Устанавливаем время с помощью соответствующих селекторов
   // А также устанавливаем интервал в 1000 миллисек.
-  function setClock (selector, endTime) {
+  function setClock(selector, endTime) {
     const timer = document.querySelector(selector),
-          days = timer.querySelector('#days'),
-          hours = timer.querySelector('#hours'),
-          minutes = timer.querySelector('#minutes'),
-          seconds = timer.querySelector('#seconds'),
-          timeInterval = setInterval(updateClock, 1000);
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
 
-    updateClock(); // вызываем ф-цию для того, чтобы после обновления страницы выводилось сразу 
+    updateClock(); // вызываем ф-цию для того, чтобы после обновления страницы выводилось сразу
     // нужное время, а не то которое прописано в вёрстке
 
     // Создаем переменную t в виде объекта оставшегося времени,
@@ -86,12 +94,12 @@ window.addEventListener('DOMContentLoaded', () => {
     function updateClock() {
       const t = getTimeRemaining(endTime);
 
-      days.innerHTML = t.days;
-      hours.innerHTML = t.hours;
-      minutes.innerHTML = t.minutes;
-      seconds.innerHTML = t.seconds;
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
-      if(t.total <= 0) {
+      if (t.total <= 0) {
         clearInterval(timeInterval);
       }
     }
