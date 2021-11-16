0f7e64cb2c29ddb5cd889e1b4064e2a7c5c24fa2
pacman766 <3334976@gmail.com>
2021-11-16 12:27:25 +0300

    1611 1227

--- js/script.js
+++ js/script.js
@@ -161,7 +161,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   window.addEventListener('scroll', showModalByScroll);
 
-  // Используем классы для карточек
+  // Карточки (работа с сервером)
 
   class MenuCard {
     constructor(
@@ -215,34 +215,33 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // ф-ция получения данных с сервера. Делаем запрос на сервер,
+  // дожидаемся окончания, обрабатываем ошибку и трансформируем в json
+  const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  //
+  getResource('http://localhost:3000/menu').then((data) => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        '.menu .container'
+      ).render();
+    });
+  });
   // создаем объект с соответствущими каждому блоку параметрами и вызываем
   // на нем метод render()
-  new MenuCard(
-    'img/tabs/vegy.jpg',
-    'vegy',
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Этоабсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    '.menu .container'
-  ).render();
-
-  new MenuCard(
-    'img/tabs/elite.jpg',
-    'elite',
-    'Меню “Премиум”',
-    'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    14,
-    '.menu .container'
-  ).render();
-
-  new MenuCard(
-    'img/tabs/post.jpg',
-    'post',
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    21,
-    '.menu .container'
-  ).render();
 
   // Forms (работа с сервером)
 
@@ -255,14 +254,28 @@ window.addEventListener('DOMContentLoaded', () => {
     failure: 'Что-то пошло не так...',
   };
 
-  // на каждую форму подвязываем ф-цию postData
+  // на каждую форму подвязываем ф-цию bindPostData
   forms.forEach((item) => {
-    postData(item);
+    bindPostData(item);
   });
+
+  // ф-ция отправки запроса на сервер, получение ответа с сервера в виде
+  // promise и далее возвращает и конвертирует в json
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: 'POST', // каким образом
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: data, // что именно
+    });
+
+    return await res.json();
+  };
 
   // отправка данных пол-ля на сервер и ответ с сервера в виде
   // вспылвающего окна
-  function postData(form) {
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault(); // откл дэфолтное поведение браузера
 
@@ -276,20 +289,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
       const formData = new FormData(form); // формируем данные которые задал пол-тель
 
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value; // переносим данные в объект
-      });
+      // 1. в массив массивов, 2. в объект, 3. в JSON
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      fetch('server.php', {
-        // куда
-        method: 'POST', // каким образом
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(object), // что именно
-      })
-        .then((data) => data.text()) // promise
+      // отправляем запрос с данными json на сервер, с помощью промисов
+      // обрабатываем данные от пол-ля (выводим данные в консоль, отобращаем
+      // showThanksModal, удаляем спиннер), обрабатываем неудачный рез-т,
+      // очищаем форму
+      postData('http://localhost:3000/requests', json)
         .then((data) => {
           console.log(data);
           showThanksModal(message.success);
@@ -299,7 +306,7 @@ window.addEventListener('DOMContentLoaded', () => {
           showThanksModal(message.failure);
         })
         .finally(() => {
-          form.reset(); // очищаем форму
+          form.reset();
         });
     });
   }
@@ -333,8 +340,4 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
-
-  fetch('http://localhost:3000/menu')
-    .then((data) => data.json())
-    .then((res) => console.log(res));
 });
